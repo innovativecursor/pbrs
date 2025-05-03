0662520e49c686755ac21eaadd3c2c0ee4aae5ff
vaish567 <vaishnav.parte@gmail.com>
2025-05-03 17:55:33 +0530

    update

--- src/app/(frontend)/components/ui/CityImageCard.tsx
+++ src/app/(frontend)/components/ui/CityImageCard.tsx
@@ -12,10 +12,12 @@ const CityImageCard: React.FC<CityImageCardProps> = ({ img, cityName, index }) =
       <Image
         src={typeof img === 'string' ? img : img.src}
         alt={`City ${index + 1}`}
-        width={300}
-        height={200}
-        className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-105"
+        width={0} // width: 0 for auto scaling
+        height={0} // height: 0 for auto scaling
+        layout="responsive" // Makes the image responsive to its container size
+        className="object-cover w-full h-full transform transition-transform duration-300 group-hover:scale-105"
       />
+
       <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-b from-black/40 via-transparent to-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <span className="text-white font-semibold text-lg translate-y-4 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500 ease-in-out">
           {cityName}
