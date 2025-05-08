71ba8e481421ef158a9418c9965bd229b77cc7b1
vaish567 <vaishnav.parte@gmail.com>
2025-05-08 17:15:57 +0530

    update

--- src/app/(frontend)/components/ContactSection.tsx
+++ src/app/(frontend)/components/ContactSection.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import { FaMailBulk, FaPhone } from 'react-icons/fa'
 import Image from 'next/image'
 import contactGroup from '../public/assets/propertiesHero/contact_group.png'
 import vectorgrp from '../public/assets/propertiesHero/vector_17.png'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
 
 const ContactSection = () => {
   return (
@@ -30,7 +31,7 @@ const ContactSection = () => {
           <div className="w-full md:w-1/2 text-center md:text-left p-4">
             <h2 className="text-3xl md:text-4xl font-bold text-black leading-snug">
               Contact Us for More <br />
-              Exclusive <span className="text-purple-600 underline">Offers</span>
+              Exclusive <span className="text-[#CB6ABA] underline">Offers</span>
             </h2>
           </div>
         </div>
@@ -42,7 +43,7 @@ const ContactSection = () => {
             <p className="text-lg font-semibold">Call:</p>
             <hr className="my-2 border-purple-300" />
             <div className="flex items-center gap-3 mt-2 text-lg">
-              <FaPhone className="w-5 h-5" />
+              <FaPhoneAlt className="w-5 h-5" />
               <span>+63 910 526 6020</span>
             </div>
           </div>
@@ -52,7 +53,7 @@ const ContactSection = () => {
             <p className="text-lg font-semibold">Email:</p>
             <hr className="my-2 border-purple-300" />
             <div className="flex items-center gap-3 mt-2 text-lg">
-              <FaMailBulk className="w-5 h-5" />
+              <MdEmail className="w-5 h-5" />
               <span>paulbalita7@gmail.com</span>
             </div>
           </div>
