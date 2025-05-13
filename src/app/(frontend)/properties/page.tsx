f655ce890b78d4654ed660767c0e0dbebba44ef2
vaish567 <vaishnav.parte@gmail.com>
2025-05-13 15:03:07 +0530

    update

--- src/app/(frontend)/properties/page.tsx
+++ src/app/(frontend)/properties/page.tsx
@@ -1,7 +1,11 @@
 import { Suspense } from 'react'
 import PropertiesPage from './PropertiesPageWrapper'
 import Loader from '../components/ui/Loader' // Adjust the path if needed
-
+export const metadata = {
+  title: 'Properties | Paul Balita Realty Services',
+  description:
+    'Browse properties for sale and rent at Paul Balita Realty Services. Filter by price, location, and more.',
+}
 export default function PropertiesPageRoute() {
   return (
     <Suspense
